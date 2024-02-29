5085939bf147310d3572746623e557d44bfdf9d8
nethreen <nezrinsoltanli@gmail.com>
2024-02-29 15:54:33 +0400

    all page

--- script/swiper.js
+++ script/swiper.js
@@ -7,3 +7,12 @@ var swiper = new Swiper(".blogSwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+var swiper = new Swiper(".blogDetail", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
