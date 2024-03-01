1981af015eca4c63d3ea771dd48ccf3c245e6009
nethreen <nezrinsoltanli@gmail.com>
2024-03-01 16:08:41 +0400

    mobile

--- script/swiper.js
+++ script/swiper.js
@@ -6,8 +6,19 @@ var swiper = new Swiper(".blogSwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
 });
-var swiper = new Swiper(".blogDetail", {
+var swiper1 = new Swiper(".blogDetail", {
   pagination: {
     el: ".swiper-pagination",
   },
