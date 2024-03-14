e9849b85cd11c69f2204bb1bb0b8fdf32f7838f8
nethreen <nezrinsoltanli@gmail.com>
2024-03-14 13:04:25 +0400

    last update, all pages

--- script/main.js
+++ script/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+  $(window).scroll(function () {
+    30 <= window.pageYOffset
+      ? $("header").addClass("active")
+      : $("header").removeClass("active");
+  });
   $(".menuButton").click(function () {
     $(".menuPopup").addClass("active");
     $("body").addClass("body-overflow");
@@ -14,5 +19,15 @@ $(document).ready(function () {
   $(".sendEmail").click(function () {
     $(".popup").addClass("active");
     $("body").addClass("body-overflow");
+  });
+  $(".dropdownHead").click(function () {
+    $(".dropdownBody").toggleClass("active");
+  });
+  $(document).click(function (e) {
+    e.stopPropagation();
+    var container = $(".dropdown");
+    if (container.has(e.target).length === 0) {
+      $(".dropdownBody").removeClass("active");
+    }
   });
 });
